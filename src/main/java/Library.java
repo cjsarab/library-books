0a3193432504ea6532f8ed5a5b3ea17d392322f2
Cammy Smith <cjsarab1991@gmail.com>
2022-11-16 06:31:25 +0000

    can't run files

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collection;
+
+    public Library () {
+        this.collection = new ArrayList<Book>();
+    }
+
+    public int countBooks() {
+        return this.collection.size();
+    }
+
+    public void addBook(Book book1) {
+        collection.add(book1);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,5 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book1 = new Book("LotR", "JRR Tolkien", "Fantasy");
+        book2 = new Book("Long Walk to Freedom", "Nelson Mandela", "Autobiography");
+    }
+
+    @Test
+    public void libraryStartsEmpty() {
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBookToLibrary() {
+        library.addBook(book1);
+        assertEquals(1, library.countBooks());
+    }
+
 
 }
